06983894f7caca27958099507a69f2a11ac363b8
Elena Muratova <ysmur@yandex.ru>
2022-12-02 16:52:29 +0300

    урок 3 Клетчатое поле _задача Инициализация

---
+++ PG/U3_Клетчатое поле/инициализация.py
@@ -0,0 +1,54 @@
+import pygame
+
+
+class Board:
+    # создание поля
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        # значения по умолчанию
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    # настройка внешнего вида
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, screen):
+        for x in range(self.width):
+            for y in range(self.height):
+                pygame.draw.rect(screen,
+                                 pygame.Color('blue'),
+                                 (x * self.cell_size + self.left,
+                                  y * self.cell_size + self.top,
+                                  self.cell_size,
+                                  self.cell_size),
+                                 2)
+
+
+def main():
+    pygame.init()
+    size = 500, 400
+    screen = pygame.display.set_mode(size)
+
+    board = Board(5, 4)
+    board.set_view(50, 50, 50)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        board.render(screen)
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
