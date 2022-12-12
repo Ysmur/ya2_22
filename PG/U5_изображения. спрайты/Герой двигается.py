2db6c607dd9df865740e400a94c48f7b58903822
Elena Muratova <ysmur@yandex.ru>
2022-12-12 20:05:02 +0300

    урок 5 изображения и спрайты _задача Герой двигается (частичное решение)

---
+++ PG/U5_изображения. спрайты/Герой двигается.py
@@ -0,0 +1,69 @@
+import os
+import random
+import sys
+
+import pygame
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Hero(pygame.sprite.Sprite):
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = load_image('creature.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
+
+    def update(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+
+
+
+
+def main():
+    pygame.init()
+    size = width, height = 501, 501
+    screen = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
+    FPS = 60
+
+    heros = pygame.sprite.Group()
+    hero = Hero(heros)
+
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                key = pygame.key.get_pressed()
+                if key[pygame.K_DOWN]:
+                    hero.update(hero.rect.x, hero.rect.y + 10)
+
+        screen.fill((255, 255, 255))
+        heros.draw(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
