96f89f751649014d6764ada31160cba0377ad970
Elena Muratova <ysmur@yandex.ru>
2022-12-16 17:59:54 +0300

    урок 6 столкновения_взаимодействия _в задаче Десант (полностью)

--- PG/U6_Столкновения_взаимодействия/десант.py
+++ PG/U6_Столкновения_взаимодействия/десант.py
@@ -25,11 +25,34 @@ class Mountain(pygame.sprite.Sprite):
         # располагаем горы внизу
         self.rect.bottom = app.height
 
+
+class Landing(pygame.sprite.Sprite):
+    image = load_image("pt.png")
+
+    def __init__(self, app, pos):
+        super().__init__(app.all_sprites)
+        self.app = app
+        self.image = Landing.image
+        self.rect = self.image.get_rect()
+        # вычисляем маску для эффективного сравнения
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    # def update(self):
+    #     self.rect = self.rect.move(0, 1)
+    def update(self):
+        # если ещё в небе
+        if not pygame.sprite.collide_mask(self, self.app.mountain):
+            self.rect = self.rect.move(0, 1)
+
+
 class App:
     def __init__(self):
         pygame.init()
         size = self.width, self.height = 501, 501
         self.screen = pygame.display.set_mode(size)
+        pygame.display.set_caption('desant')
         self.clock = pygame.time.Clock()
         self.FPS = 60
         self.all_sprites = pygame.sprite.Group()
@@ -42,6 +65,8 @@ class App:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    Landing(self, event.pos)
 
             self.screen.fill((255, 255, 255))
             self.all_sprites.draw(self.screen)
