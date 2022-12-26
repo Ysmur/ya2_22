bbea55cc4aeae4dcdfd44e878d1d7344ff9cd747
Elena Muratova <ysmur@yandex.ru>
2022-12-26 18:27:30 +0300

    урок 8 Украшение игры - Анимация

---
+++ PG/U8_Украшения игры/анимация.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import pygame
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, app, sheet, columns, rows, x, y):
+        super().__init__(app.all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+
+class App:
+    def __init__(self):
+        pygame.init()
+        self.width, self.height = 600, 600
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('Mario')
+        self.fps = 10
+        self.all_sprites = pygame.sprite.Group()
+        self.dragon = AnimatedSprite(self, self.load_image("dragon_sheet8x2.png"), 8, 2, 50, 50)
+
+    def terminate(self):
+        pygame.quit()
+        sys.exit()
+
+    def load_image(self, name, colorkey=None):
+        fullname = os.path.join('data', name)
+        # если файл не существует, то выходим
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        if colorkey is not None:
+            image = image.convert()
+            if colorkey == -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+        return image
+
+    def run_game(self):
+        run = True
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.terminate()
+            # update
+
+            # render
+            self.screen.fill(pygame.Color('blue'))
+            self.all_sprites.draw(self.screen)
+            self.all_sprites.update()
+
+            pygame.display.flip()
+            self.clock.tick(self.fps)
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run_game()
